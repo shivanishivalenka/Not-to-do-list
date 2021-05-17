ee47f788632e47505ad5486d5d206a1276f82b65
shivanishivalenka <shivalenkashivani@gmail.com>
2021-05-17 20:26:13 +0530

    enter btn event

--- src/Search.js
+++ src/Search.js
@@ -2,11 +2,17 @@
 
 const Search = (props) =>{
     const {addItem,text,setText,inputerror} = props;
+    const handelKeyPress = (e) =>{
+        if (e.keyCode === 13) {
+            e.target.click();
+        }
+    }
     return (
         <div>
             <input type = "text" 
             value = {text}
-            onChange = {(e)=>setText(e.target.value)}></input>
+            onChange = {(e)=>setText(e.target.value)}
+            onKeyPress = {handelKeyPress}></input>
             <button onClick = {addItem} >ADD </button>
             <p>{inputerror}</p>
         </div>
